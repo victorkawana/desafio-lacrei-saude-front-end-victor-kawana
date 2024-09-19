44b42496b218f8fc81a5a1d3c668b24c4df08a1e
Victor Kawana <victorkawana10@gmail.com>
2024-09-19 12:37:23 -0300

    Update page.tsx

--- lacrei-front-end/app/page.tsx
+++ lacrei-front-end/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
