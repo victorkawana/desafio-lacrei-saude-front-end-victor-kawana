cff03329336e56f5937a48a90815512b3783bf5f
Victor Kawana <victorkawana10@gmail.com>
2024-09-19 13:12:22 -0300

    Update not-found.tsx

--- lacrei-front-end/app/not-found.tsx
+++ lacrei-front-end/app/not-found.tsx
@@ -1,13 +1,11 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Custom404 } from '@/pages/404';
+import Custom404 from '@/pages/404';
 
 export default function NotFound() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <Custom404 />
-      <Footer />
     </div>
   );
 }
